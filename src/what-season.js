7da3aee1725a0dfcbbbad9282f17c1af15cb1885
Denys Bilonozhko <deformix@gmail.com>
2021-02-10 20:51:07 +0200

    fix: what season last test

--- src/what-season.js
+++ src/what-season.js
@@ -1,25 +1,27 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 module.exports = function getSeason(date) {
-  if (date == undefined) {
+  if (date === undefined) {
     return 'Unable to determine the time of year!';
+  } else if (isNaN(date)) {
+    throw new Error();
   }
 
   let month = date.getMonth();
 
-    if ( month == 0 || month == 1 || month == 11) {
-      return 'winter';
-    }
+  if (month == 0 || month == 1 || month == 11) {
+    return 'winter';
+  }
 
-    if ( month == 2 || month == 3 || month == 4) {
-      return 'spring'; 
-    }
+  if (month == 2 || month == 3 || month == 4) {
+    return 'spring';
+  }
 
-    if ( month == 5 || month == 6 || month == 7) {
-      return 'summer';
-    }
+  if (month == 5 || month == 6 || month == 7) {
+    return 'summer';
+  }
 
-    if ( month == 8 || month == 9 || month == 10) {
-      return 'autumn'; 
-    } 
+  if (month == 8 || month == 9 || month == 10) {
+    return 'autumn';
+  }
 };
