05348b4f376b97d1204a5a3e71b3ba50b59b36bb
Denys Bilonozhko <deformix@gmail.com>
2021-03-02 22:50:02 +0200

    feat: add transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,48 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error('Not an array');
+  }
+
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next': {
+        i++;
+
+        break;
+      }
+
+      case '--discard-prev': {
+        if (arr[i - 2] !== '--discard-next') {
+          result.pop();
+        }
+
+        break;
+      }
+
+      case '--double-next': {
+        if (arr[i + 1] !== undefined) {
+          result.push(arr[i + 1]);
+        }
+
+        break;
+      }
+      
+      case '--double-prev': {
+        if (arr[i - 2] !== '--discard-next' && arr[i - 1] !== undefined) {
+          result.push(arr[i - 1]);
+        }
+
+        break;
+      }
+
+      default:
+        result.push(arr[i]);
+    }
+  }
+
+  return result;
 };
